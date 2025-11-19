084d2e3cefa072526e6e049d3cae60e730dbc222
Abigail Unwin <abigailshooper@gmail.com>
2025-11-19 20:08:20 +0000

    Add models and dropdowns to select product

---
+++ Shared/ChatRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolicyChatbot.Shared
+{
+    public class ChatRequest
+    {
+        public string ProductId { get; set; } = "";
+        public string Question { get; set; } = "";
+    }
+}
